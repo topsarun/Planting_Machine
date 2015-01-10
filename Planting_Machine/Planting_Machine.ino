70fdbf8ab25a013cfc129c65713ef442ce81d75f
topsarun <topargagr2@gmail.com>
2015-01-11 03:05:41 +0700

    Add Pinmap & change pin

--- Planting_Machine/Planting_Machine.ino
+++ Planting_Machine/Planting_Machine.ino
@@ -1,3 +1,22 @@
+/*
+PinMap
+A0 Ready lamp
+A1 Too Fast Lamp
+A2 Too Slow Lamp
+A3 Buzzer
+A4 I2C Bus SDA
+A5 I2C Bus SCL
+0  Serial RX
+1  Serial TX
+2  Encoder 1 Pharse A
+3  Encoder 2 Pharse A
+4  Encoder 1 Pharse B
+5  Encoder 2 Pharse B
+6  PWM To Moter Mosfet
+7  Reset Switch
+8  Increase Space Switch
+9  Decrease Space Switch
+*/
 #include <pt.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
@@ -12,23 +31,19 @@
 #define MAXSTATE 2;
 #define SW_RESET_ON 0;
 #define SW_RESET_OFF 1;
-
 struct pt pt_ReadyState;
 struct pt pt_LCDDisplay;
 struct pt pt_SoundBuzzer;
 struct pt pt_SpaceSelector;
 struct pt pt_ResetButton;
-
 int stateLed = TOOSLOW;
 int stateResetButton = 0;
-LiquidCrystal_I2C lcd(0x20,16,2);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 PT_THREAD(ReadyState(struct pt* pt))
 {
   static uint32_t ts;
- 
   PT_BEGIN(pt);
- 
   while (1)
   {
     setLedNumber();
@@ -42,9 +57,7 @@ PT_THREAD(ReadyState(struct pt* pt))
 PT_THREAD(SoundBuzzer(struct pt* pt))
 {
   static uint32_t ts;
- 
   PT_BEGIN(pt);
- 
   while (1)
   {
     tone(A3, 400);
@@ -58,14 +71,12 @@ PT_THREAD(SoundBuzzer(struct pt* pt))
 PT_THREAD(SpaceSelector(struct pt* pt))
 {
   static uint32_t ts;
- 
   PT_BEGIN(pt);
- 
   while (1)
   {
-    if(digitalRead(A4) == LOW) {
+    if(digitalRead(8) == LOW) {
       //increase space
-    } else if(digitalRead(A5) == LOW) {
+    } else if(digitalRead(9) == LOW) {
       //decrease space
     }
   }
@@ -75,9 +86,7 @@ PT_THREAD(SpaceSelector(struct pt* pt))
 PT_THREAD(LCDDisplay(struct pt* pt))
 {
   static uint32_t ts;
- 
   PT_BEGIN(pt);
- 
   while (1)
   {
       lcd.setCursor(0,0);
@@ -95,9 +104,7 @@ PT_THREAD(LCDDisplay(struct pt* pt))
 PT_THREAD(ResetButton(struct pt* pt))
 {
   static uint32_t ts;
- 
   PT_BEGIN(pt);
- 
   while (1)
   {
     if(digitalRead(7) == HIGH) {
@@ -125,12 +132,11 @@ void setup()
   pinMode(A0, OUTPUT); //For LED Ready
   pinMode(A1, OUTPUT); //For LED Too Fast
   pinMode(A2, OUTPUT); //For LED Too Slow
-  pinMode(A4, INPUT_PULLUP); //For increase space
-  pinMode(A5, INPUT_PULLUP); //For decrease space
   pinMode(7, INPUT_PULLUP); //For Switch Reset
+  pinMode(8, INPUT_PULLUP); //For increase space
+  pinMode(9, INPUT_PULLUP); //For decrease space
   lcd.begin();
   lcd.backlight();
-  
   PT_INIT(&pt_ReadyState);
   PT_INIT(&pt_LCDDisplay);
   PT_INIT(&pt_SoundBuzzer);
